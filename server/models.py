0c1ccf7a34efb9e268aa639500b121e0a736d15c
Paulshapwala <paulokuyo80@gmail.com>
2025-04-02 12:49:35 +0300

    set up db schema

---
+++ server/models.py
@@ -0,0 +1,71 @@
+from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey, DateTime
+from sqlalchemy.orm import relationship, sessionmaker, Mapped, mapped_column
+from sqlalchemy.sql import func
+from setup import Base, Session
+import datetime
+
+class User(Base):
+    __tablename__ = "users"
+    id:Mapped[int] = mapped_column(primary_key=True)
+    username:Mapped[str] = mapped_column(String(64))
+    isAdmin:Mapped[bool]
+       # relationships
+    password:Mapped["Password"] = relationship(back_populates="user",cascade="all, delete-orphan")
+    transactions:Mapped[list["Transaction"]] = relationship(back_populates="user")
+
+    def __repr__(self):
+       return f"user:{self.username!r} id:{self.id!r} admin:{self.isAdmin!r}"
+    
+
+class Password(Base):
+    __tablename__ = "passwords"
+    id:Mapped[int]  = mapped_column(primary_key=True)
+    password_hash:Mapped[str] = mapped_column(String(64))
+    userId:Mapped[int] = mapped_column(ForeignKey("users.id"))
+    # relationships
+    user:Mapped["User"] = relationship(back_populates="password", uselist=False)
+    def __repr__(self):
+        return f"userId:{self.userId!r} password:{self.password_hash!r}"
+
+
+class Product(Base):
+    __tablename__ = "products"
+    id:Mapped[int] = mapped_column(primary_key=True)
+    name:Mapped[str] = mapped_column(String(64))
+    category:Mapped[str] = mapped_column(String(32))
+    quantity:Mapped[int]
+    price:Mapped[float]
+
+    # relationships
+    transactionitems:Mapped[list["TransactionItem"]] = relationship(back_populates="product")
+    
+    def __repr__(self):
+        return f"productId:{self.id!r} name:{self.name!r} category:{self.category!r}"
+    
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+    id:Mapped[int] = mapped_column(primary_key=True)
+    userId:Mapped[int] = mapped_column(ForeignKey("users.id"))
+    timeStamp:Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), default=func.now())
+    # relationships
+    items: Mapped[list["TransactionItem"]] = relationship(back_populates="transaction", cascade="all, delete-orphan")
+    user:Mapped["User"] = relationship(back_populates="transactions")
+
+    def __repr__(self):
+        return f"transactionId:{self.id!r} time:{self.timeStamp!r}"
+
+class TransactionItem(Base):
+    __tablename__ = "transactionItems"
+    id:Mapped[int] = mapped_column(primary_key=True)
+    productId:Mapped[int] = mapped_column(ForeignKey("products.id"))
+    transactionId:Mapped[int] = mapped_column(ForeignKey("transactions.id"))
+    priceAtTime:Mapped[float]
+    quantity:Mapped[int]
+    # relationships
+    product:Mapped["Product"] = relationship(back_populates="transactionitems")
+    transaction:Mapped["Transaction"] = relationship(back_populates="items")
+
+    def __repr__(self):
+        return f"itemId:{self.id!r} quantity:{self.quantity!r} price:{self.priceAtTime!r}"
+
---
+++ server/setup.py
@@ -0,0 +1,20 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+env_path = Path(__file__).resolve().parent / '.env'
+load_dotenv(dotenv_path=env_path)
+
+class Base(DeclarativeBase):
+    metadata = MetaData(naming_convention={
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    })
+
+engine = create_engine(os.environ["DATABASE_URL"])
+Session = sessionmaker(engine)
